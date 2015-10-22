4861e275a3e91c3382a5bc4a758718d44a43bbc6
ayeowch <ayeowch@gmail.com>
2015-10-22 16:35:08 +1100

    Set requests `verify=False` for api.coindesk.com as their cert has yet to be renewed

--- hardware/api/tasks.py
+++ hardware/api/tasks.py
@@ -86,7 +86,7 @@ def exchange_rate_task():
     url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
     headers = {'user-agent': settings.USER_AGENT}
     try:
-        response = requests.get(url, headers=headers, timeout=settings.HTTP_TIMEOUT)
+        response = requests.get(url, headers=headers, verify=False, timeout=settings.HTTP_TIMEOUT)
     except requests.exceptions.RequestException as err:
         logger.debug(err)
     else:
